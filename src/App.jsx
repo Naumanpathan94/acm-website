3d25c4b63daf369eacd49f6d3fa2dd0a81d92707
Naumanpathan94 <naumanpathan78@gmail.com>
2025-07-26 21:55:52 +0530

    almost there

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import RaitACMWebsite from './RaitACMWebsite';
 import './App.css';
 
 export default function App() {
-  return <RaitACMWebsite />;
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
+  
+  return (
+    <div className="app-container">
+      <RaitACMWebsite 
+        isMobile={isMobile}
+        isTablet={isTablet}
+      />
+    </div>
+  );
 }
